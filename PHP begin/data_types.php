d21712668231d2875d6b21cc6f1ed07228169ee1
Usmon <usmonzaripov@gmail.com>
2018-12-11 21:25:51 +0500

    PHP data types

---
+++ PHP begin/data_types.php
@@ -0,0 +1,30 @@
+<?php
+
+$var_one = 45; //integer
+$var_two = true; //bool
+$var_three = array(4,5,6); //array
+$var_four = 15.5; //double
+$var_five = NULL; //NULL
+$var_six = "STRING"; // string
+$var_seven = new DateTime(); //object
+
+//echo gettype($var_one);
+//echo gettype($var_two);
+//echo gettype($var_three);
+//echo gettype($var_four);
+//echo gettype($var_five);
+//echo gettype($var_six);
+//echo gettype($var_seven);
+
+//if( $var_two === true ){
+//    echo "bu true";
+//}
+
+//if(isset($var_five))
+//{
+//    echo 'qiymati mavjud';
+//}
+
+//echo $var_four;
+//unset($var_four); // <=> $var_four = NULL;
+//echo gettype($var_four);
