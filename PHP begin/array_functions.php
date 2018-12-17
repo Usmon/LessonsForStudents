f4bc96a1e30e6a2c63cb4483cd63f11a80e45530
Usmon <usmonzaripov@gmail.com>
2018-12-17 21:19:45 +0500

    Array functions example

---
+++ PHP begin/array_functions.php
@@ -0,0 +1,15 @@
+<?php
+
+    $myArray = [5, 6, 7, 8, 30, 500];
+    $myArray2 = [25, 6];
+//    array_push($myArray, 105);
+//    $rand_index = array_rand($myArray);
+//    array_pop($myArray);
+//    echo $myArray[$rand_index];
+//    echo $myArray[$rand_index];
+//    var_dump(array_diff($myArray, $myArray2));
+//    var_dump(array_chunk($myArray, 2));
+//    var_dump(array_reverse($myArray));
+//    var_dump( array_search(500, $myArray) );
+//    var_dump( array_sum($myArray) );
+//    var_dump($myArray);
