22f07fa7b123249be16d2d4f0fa025775c2090df
Usmon <usmonzaripov@gmail.com>
2018-12-21 21:47:54 +0500

    Count with search and union array

---
+++ PHP begin/count_str.php
@@ -0,0 +1,5 @@
+<?php
+    $text = "Lorem ipsum dolor sit home do todo";
+    $need = 'do';
+
+    echo substr_count($text, $need, 15);
--- PHP begin/string.php
+++ PHP begin/string.php
@@ -6,7 +6,7 @@
     var_dump($splitted_data);
 
     $array_string = ['Ot', 'Baliq', 'Tuya', 'Mushuk'];
-    $array_string = ['It', 'Laylak', 'Kabutar'];
+    $array_string2 = ['It', 'Laylak', 'Kabutar'];
 
     echo implode(', ', $array_string);
 
@@ -15,3 +15,29 @@
     echo substr_replace($name, 'new string', 5, 3);
     $replaced = str_replace(' ', '', $name);
     echo strlen($replaced);
+
+    function union_array($array_one, $array_two)
+    {
+        $result = [];
+        $max = count($array_one);
+        if($max < count($array_two))
+        {
+            $max = coun($array_two);
+        }
+        for ($i = 0; $i<$max; $i++)
+        {
+            if(isset($array_one[$i]))
+            {
+                array_push($result, $array_one[$i]);
+            }
+            if(isset($array_two[$i]))
+            {
+                array_push($result, $array_two[$i]);
+            }
+        }
+
+        return $result;
+    }
+
+    var_dump( union_array($array_string, $array_string2) );
+
