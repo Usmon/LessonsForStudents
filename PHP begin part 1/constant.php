de8e5d073710b105153b86dea33e5a6d43d71ed0
Usmon <usmonzaripov@gmail.com>
2019-02-13 20:52:25 +0500

    Constants - DEFINE

---
+++ PHP begin part 1/constant.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Usmon
+ * Date: 13.02.2019
+ * Time: 20:37
+ */
+echo DIRECTORY_SEPARATOR;
+
+define('PI', 3.14);
+define('e', 2.71);
+echo M_PI;
+echo M_E;
+$varA = 15;
+echo e;
+
